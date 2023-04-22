979cc55458196d355a14ab7301a0fbb9e225ee56
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-04-22 16:19:08 -0300

    add frontend with bootstrap

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  public isPasswordVisible = false;
 
+  constructor() {}
+
+  togglePasswordVisibility() {
+    const passwordField = document.getElementById(
+      'form1Example23'
+    ) as HTMLInputElement;
+    if (passwordField !== null) {
+      passwordField.type =
+        passwordField.type === 'password' ? 'text' : 'password';
+      const showPasswordIcon = document.getElementById('show-password-icon');
+      if (showPasswordIcon !== null) {
+        showPasswordIcon.classList.toggle('fa-eye-slash');
+      }
+    }
+  }
 }
