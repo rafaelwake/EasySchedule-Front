0523a4e2fa89757965e8dde8d161d7cef3cd5dc4
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-04-22 21:31:39 -0300

    add css and html at page register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { Router } from '@angular/router';
+import { FormValidationService } from 'src/app/services/form-validation/form-validation.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  public isPasswordVisible = false;
+  public error = '';
+  showError: boolean = false;
+  public email: string = '';
+  public password: string = '';
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private formValidationService: FormValidationService
+  ) {}
+
+  togglePasswordVisibility() {
+    const passwordField = document.getElementById(
+      'password'
+    ) as HTMLInputElement;
+    if (passwordField !== null) {
+      passwordField.type =
+        passwordField.type === 'password' ? 'text' : 'password';
+      const showPasswordIcon = document.getElementById('show-password-icon');
+      if (showPasswordIcon !== null) {
+        showPasswordIcon.classList.toggle('fa-eye-slash');
+      }
+    }
+  }
+
+  onSubmit() {
+    const emailError = this.formValidationService.validateEmail(this.email);
+    const passwordError = this.formValidationService.validatePassword(
+      this.password
+    );
+
+    if (emailError) {
+      this.showError = true;
+      this.error = emailError;
+      setTimeout(() => {
+        this.showError = false;
+      }, 2000);
+      return;
+    }
+
+    if (passwordError) {
+      this.showError = true;
+      this.error = passwordError;
+      setTimeout(() => {
+        this.showError = false;
+      }, 2000);
+      return;
+    }
+
+    console.log(this.email, this.password);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
+  redirectToRecovery() {
+    this.router.navigate(['/recovery']);
+  }
 }
