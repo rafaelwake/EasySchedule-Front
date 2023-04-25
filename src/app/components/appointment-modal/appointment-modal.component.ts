83a1d614ce79ac836160011de884946ae1401caa
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-04-25 13:23:57 -0300

    add base config at appointment-modal

--- src/app/components/appointment-modal/appointment-modal.component.ts
+++ src/app/components/appointment-modal/appointment-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AppointmentModel } from 'src/app/models/appointment.model';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateAppointmentsService } from 'src/app/services/scheduling/create-appointments.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-modal',
@@ -11,25 +12,38 @@ import { CreateAppointmentsService } from 'src/app/services/scheduling/create-ap
 export class AppointmentModalComponent implements OnInit {
   @Input() appointment!: AppointmentModel;
   @Output() saveAppointment = new EventEmitter<AppointmentModel>();
+  form!: FormGroup;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private createAppointmentsService: CreateAppointmentsService
+  ) {}
 
-  ngOnInit(): void {}
-
-  save() {
-    this.saveAppointment.emit(this.appointment);
-    this.activeModal.close();
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: [this.appointment.title, Validators.required],
+      description: [this.appointment.description, Validators.required],
+      // Adicione outros campos conforme necessário
+    });
   }
 
-  // onSubmit() {
-  //   if (form.valid) {
-  //     this.createAppointmentsService
-  //       .createAppointment(this.appointment)
-  //       .subscribe(
-  //         (res) => {
-  //           console.log(res);
-  //         },
-  //         (err) => console.error(err)
-  //       );
-  //   }
+  onSubmit() {
+    if (this.form.valid) {
+      this.appointment.title = this.form.get('title')?.value;
+      this.appointment.description = this.form.get('description')?.value;
+      // Atualize outros campos conforme necessário
+
+      this.createAppointmentsService
+        .createAppointment(this.appointment)
+        .subscribe(
+          (res) => {
+            console.log(res);
+            this.saveAppointment.emit(this.appointment);
+            this.activeModal.close();
+          },
+          (err) => console.error(err)
+        );
+    }
+  }
 }
--- src/app/services/scheduling/create-appointments.service.ts
+++ src/app/services/scheduling/create-appointments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AppointmentModel } from 'src/app/models/appointment.model';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,16 @@ export class CreateAppointmentsService {
   constructor(private http: HttpClient) {}
 
   createAppointment(appointment: AppointmentModel) {
-    return this.http.post('http://localhost:3000/appointments', appointment);
+    //return this.http.post('http://localhost:3000/appointments', appointment);
+
+    // Simula uma resposta de sucesso
+    const response = {
+      status: 'success',
+      message: 'Appointment created successfully.',
+      data: appointment,
+    };
+
+    // Retorna um Observable com a resposta simulada
+    return of(response);
   }
 }
