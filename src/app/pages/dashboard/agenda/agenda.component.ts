49289c01706d4c0caebab4cd93ab6daa9e6a204d
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-05-01 10:42:21 -0300

    fix environment

--- src/app/pages/dashboard/agenda/agenda.component.ts
+++ src/app/pages/dashboard/agenda/agenda.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionModel } from 'src/app/models/user.model';
+import { SessionService } from 'src/app/services/user/session.service';
+import { Router } from '@angular/router';
+import { UpdateAppointmentsService } from 'src/app/services/scheduling/update-appointments.service';
+
 @Component({
   templateUrl: './agenda.component.html',
-  styleUrls: ['./agenda.component.css']
+  styleUrls: ['./agenda.component.css'],
 })
-export class AgendaComponent {
+export class AgendaComponent implements OnInit {
+  session: SessionModel;
+  appointments: any[] = [];
 
+  constructor(
+    private sessionService: SessionService,
+    private router: Router,
+    private updateAppointmentsService: UpdateAppointmentsService
+  ) {
+    this.session = sessionService.getSession();
+  }
+
+  ngOnInit() {
+    this.updateAppointmentsService
+      .getAppointments(this.session.token)
+      .subscribe(
+        (response) => {
+          this.appointments = response.data;
+          console.log('appointments', this.appointments);
+        },
+        (error) => {
+          console.error('Erro ao obter compromissos:', error);
+        }
+      );
+  }
+
+  parseDate(dateStr: string): Date {
+    const [datePart, timePart] = dateStr.split(' ');
+    const [year, month, day] = datePart.split('-').map(Number);
+    const [hour, minute] = timePart.split(':').map(Number);
+
+    return new Date(year, month - 1, day, hour, minute);
+  }
 }
--- src/app/services/scheduling/update-appointments.service.ts
+++ src/app/services/scheduling/update-appointments.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/app/config';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UpdateAppointmentsService {
+  private baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAppointments(token: string): Observable<any> {
+    const url = `${this.baseUrl}/appointment`;
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get(url, { headers });
+  }
 }
